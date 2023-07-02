0af47895eda789d98104244a166cbfdb891fe710
Jane See <seeloojane@gmail.com>
2023-07-03 02:36:26 +0800

    Add field `crew` and update field `className` -> `starshipClass` for the Starship data class
    - We will be needing the crew field for response generation later
    - We will model Starship class after the data object from swapi, and have another object that takes care of the `/information` response structure later

--- src/main/java/com/example/demo/data/Starship.java
+++ src/main/java/com/example/demo/data/Starship.java
@@ -1,23 +1,17 @@
 package com.example.demo.data;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
-
-import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Starship {
-
-    @JsonInclude(NON_NULL)
     private String name;
-
-    @JsonInclude(NON_NULL)
-    @JsonProperty("class")
-    private String className;
-
-    @JsonInclude(NON_NULL)
+    @JsonProperty("starship_class")
+    private String starshipClass;
     private String model;
+    private String crew;
 }
--- src/test/java/com/example/demo/data/StarshipTest.java
+++ src/test/java/com/example/demo/data/StarshipTest.java
@@ -5,40 +5,26 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StarshipTest {
     ObjectMapper mapper = new ObjectMapper();
     @Test
-    public void nameOfClassFieldIsChangedAfterSerialization() throws IOException {
-        Starship starship = new Starship("some-n", "some-c", "some-m");
+    public void shouldDeserializeAllFields() throws IOException {
+        String json = """
+        {
+            "name": "someName",
+            "model": "someModel",
+            "starship_class": "someClass",
+            "crew": "someCrew"
+        }
+     """;
 
-        String serialized = mapper.writeValueAsString(starship);
+        Starship starship = mapper.readValue(json, Starship.class);
 
-        assertThat(serialized, not(containsString("className")));
-        assertThat(serialized, containsString("class"));
-    }
-
-    @Test
-    public void shouldSerializeAllFieldsIfPresent() throws IOException {
-        Starship starship = new Starship("some-n", "some-c", "some-m");
-
-        String serialized = mapper.writeValueAsString(starship);
-
-        assertThat(serialized, containsString("some-n"));
-        assertThat(serialized, containsString("some-c"));
-        assertThat(serialized, containsString("some-m"));
-    }
-    @Test
-    public void shouldNotSerializeFieldIfEmpty() throws IOException {
-        Starship starship = new Starship();
-
-        String serialized = mapper.writeValueAsString(starship);
-
-        assertThat(serialized, not(containsString("name")));
-        assertThat(serialized, not(containsString("class")));
-        assertThat(serialized, not(containsString("model")));
+        assertEquals("someName", starship.getName());
+        assertEquals("someModel", starship.getModel());
+        assertEquals("someClass", starship.getStarshipClass());
+        assertEquals("someCrew", starship.getCrew());
     }
 }
