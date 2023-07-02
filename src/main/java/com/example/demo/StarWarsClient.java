0e960875870f7d1d3f2a0bf29cbfb7d342f9d96c
Jane See <seeloojane@gmail.com>
2023-07-03 03:26:43 +0800

    Add methods to get `Person` and `Planet` resource given an ID from swapi

--- src/main/java/com/example/demo/StarWarsClient.java
+++ src/main/java/com/example/demo/StarWarsClient.java
@@ -1,16 +1,19 @@
 package com.example.demo;
 
+import com.example.demo.data.Person;
+import com.example.demo.data.Planet;
 import com.example.demo.data.Starship;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
+@Slf4j
 public class StarWarsClient {
 
     private final RestTemplate restTemplate;
@@ -22,14 +25,44 @@ public class StarWarsClient {
     public Starship getSingleStarship(int id) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-
         HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
 
         try {
             return this.restTemplate
                     .exchange("/starships/{id}", HttpMethod.GET, requestEntity, Starship.class, id)
                     .getBody();
-        } catch (HttpClientErrorException exception) {
+        } catch (Exception exception) {
+            log.error("Error occurred when invoking API to get Starship details.", exception);
+            return null;
+        }
+    }
+
+    public Person getPerson(int id) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+
+        try {
+            return this.restTemplate
+                    .exchange("/people/{id}", HttpMethod.GET, requestEntity, Person.class, id)
+                    .getBody();
+        } catch (Exception exception) {
+            log.error("Error occurred when invoking API to get Person details.", exception);
+            return null;
+        }
+    }
+
+    public Planet getSinglePlanet(int id) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+
+        try {
+            return this.restTemplate
+                    .exchange("/planets/{id}", HttpMethod.GET, requestEntity, Planet.class, id)
+                    .getBody();
+        } catch (Exception exception) {
+            log.error("Error occurred when invoking API to get Planet details.", exception);
             return null;
         }
     }
--- src/test/java/com/example/demo/StarWarsClientTest.java
+++ src/test/java/com/example/demo/StarWarsClientTest.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.example.demo.data.Person;
+import com.example.demo.data.Planet;
 import com.example.demo.data.Starship;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +11,8 @@ import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebCl
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
@@ -68,6 +72,78 @@ class StarWarsClientTest {
                 .andRespond(withResourceNotFound());
 
         Starship result = starWarsClient.getSingleStarship(notFoundId);
+
+        assertNull(result);
+    }
+
+    @Test
+    void shouldFetchPersonWithGivenId() {
+        int mockId = 1;
+        String json = """
+                   {
+                       "name": "Leia",
+                        "starships": [
+                            "https://swapi.dev/api/starships/5/"
+                        ]
+                   }
+                """;
+
+        this.mockRestServiceServer
+                .expect(requestTo("/people/" + mockId))
+                .andRespond(withSuccess(json, MediaType.APPLICATION_JSON));
+
+        Person result = starWarsClient.getPerson(mockId);
+
+        assertNotNull(result);
+        assertEquals("Leia", result.getName());
+        assertEquals(List.of("https://swapi.dev/api/starships/5/"), result.getStarships());
+    }
+
+    @Test
+    void shouldReturnNullIfPersonNotFound() {
+        int notFoundId = -1;
+
+        this.mockRestServiceServer
+                .expect(requestTo("/people/" + notFoundId))
+                .andRespond(withResourceNotFound());
+
+        Person result = starWarsClient.getPerson(notFoundId);
+
+        assertNull(result);
+    }
+
+    @Test
+    void shouldFetchPlanetWithGivenId() {
+        int mockId = 1;
+        String json = """
+                   {
+                       "name": "Alderaan",
+                        "residents": [
+                            "https://swapi.dev/api/people/5/"
+                        ]
+                   }
+                """;
+
+        this.mockRestServiceServer
+                .expect(requestTo("/planets/" + mockId))
+                .andRespond(withSuccess(json, MediaType.APPLICATION_JSON));
+
+        Planet result = starWarsClient.getSinglePlanet(mockId);
+
+        assertNotNull(result);
+        assertEquals("Alderaan", result.getName());
+        assertEquals(List.of("https://swapi.dev/api/people/5/"), result.getResidents());
+    }
+
+    @Test
+    void shouldReturnNullIfPlanetNotFound() {
+        int notFoundId = -1;
+
+        this.mockRestServiceServer
+                .expect(requestTo("/planets/" + notFoundId))
+                .andRespond(withResourceNotFound());
+
+        Planet result = starWarsClient.getSinglePlanet(notFoundId);
 
         assertNull(result);
     }
